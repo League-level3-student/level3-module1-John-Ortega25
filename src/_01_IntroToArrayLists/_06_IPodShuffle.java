8fa93fb5e1cc864e7d946f30f5e4e2ad8130fbb1
paolapadilla <league@Admins-Mac-mini.local>
2019-03-02 11:09:13 -0800

    3-2-19
    Finished iPod shuffle

--- src/_01_IntroToArrayLists/_06_IPodShuffle.java
+++ src/_01_IntroToArrayLists/_06_IPodShuffle.java
@@ -4,33 +4,30 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Random;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-
-//Copyright The League of Amazing Programmers, 2015
-
-public class _06_IPodShuffle implements ActionListener{
-	JButton button = new JButton();
+public class _06_IPodShuffle implements ActionListener {
+	JButton shuffleButton = new JButton();
 	ArrayList<Song> songs = new ArrayList<>();
-	
+	int currentSongIndex = 0;
+
 	public _06_IPodShuffle() {
 		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();	
-		songs.add(new Song("Cow.wav"));
+		JPanel panel = new JPanel();
+		songs.add(new Song("Cow.mp3"));
 		songs.add(new Song("Pig.mp3"));
-		songs.add(new Song("Rooster.wav"));
-		songs.add(new Song("Donkey.flac"));
+		songs.add(new Song("Rooster.mp3"));
+		songs.add(new Song("Donkey.mp3"));
 		songs.add(new Song("demo.mp3"));
 		frame.add(panel);
-		panel.add(button);
-		button.setText("Surprise Me!");
-		button.addActionListener(this);
+		panel.add(shuffleButton);
+		shuffleButton.setText("Surprise Me!");
+		shuffleButton.addActionListener(this);
 		frame.pack();
 		frame.setVisible(true);
-		
+
 		/**
 		 * 2. Congratulations on completing the sound check! * Now we want to make an
 		 * iPod Shuffle that plays random music. * Create an ArrayList of Songs and a
@@ -38,20 +35,22 @@ public class _06_IPodShuffle implements ActionListener{
 		 * you're really cool, you can stop all the songs, before playing a new one on
 		 * subsequent button clicks.
 		 */
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		new _06_IPodShuffle();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 		JButton buttonPressed = (JButton) e.getSource();
-		if (button == buttonPressed) {
-			System.out.println("hello");
-			songs.get(1).play();
+		if (shuffleButton == buttonPressed) {
+			songs.get(currentSongIndex).stop();
+			Random generator = new Random();
+			int newNum = generator.nextInt(songs.size());
+			currentSongIndex = newNum;
+			songs.get(newNum).play();
 		}
 	}
 }
